dc1b8c579a835e9428eeb0b92ef9657a418aa5a3
jvitormelo <jvitor.ovidio2@gmail.com>
2025-07-20 13:59:13 -0300

    Refactor Vite configuration for improved readability and functionality
    - Updated import statements to use consistent double quotes.
    - Modified the tanstackStart plugin configuration to specify the target as "vercel".

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
@@ -10,8 +10,8 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
-    tanstackStart(),
+    tanstackStart({ target: "vercel" }),
   ],
-})
+});
